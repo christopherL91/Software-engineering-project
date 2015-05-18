ae3834f445d595e132ec8f12db7a2f401f777d37
Christopher Lillthors <christopher.lillthors@gmail.com>
2015-05-18 21:26:07 +0200

    Fixed bugreport bug

--- app/components/frontdesk/bugreport/bugreport.js
+++ app/components/frontdesk/bugreport/bugreport.js
@@ -4,14 +4,24 @@
         .module('portfolioApp.bugreport',[])
         .controller('bugController',bugController);
 
-    bugController.$inject = ['$scope'];
+    bugController.$inject = ['$scope','toastr'];
 
-    function bugController($scope) {
+    function bugController($scope,toastr) {
         var vm = this;
 
         vm.sendReport = function() {
             console.log(vm.message);
+            toastr.info('We will look through your bug report and we will get back to you ASAP', ' Thank you', {
+                extendedTimeOut: 0,
+                maxOpened: 5,
+                tapToDismiss: true,
+                timeOut: 0
+            });
             $scope.closeThisDialog();
         };
+
+        vm.close = function() {
+            $scope.closeThisDialog();
+        }
     }
 })();
--- app/components/frontdesk/main/main.js
+++ app/components/frontdesk/main/main.js
@@ -64,14 +64,6 @@
                 template: '/components/frontdesk/bugreport/bugreport.html',
                 controller: 'bugController as bug'
             });
-            dialog.closePromise.then(function() {
-                toastr.info('We will look through your bug report and we will get back to you ASAP', ' Thank you', {
-                    extendedTimeOut: 0,
-                    maxOpened: 5,
-                    tapToDismiss: true,
-                    timeOut: 0
-                });
-            });
         };
 
         //Hämta senaste information om vilka inställningar som gjorts.
